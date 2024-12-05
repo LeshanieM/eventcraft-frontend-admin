ea1d3d55559ecc7c7e7f60528c83817e9d928081
LeshanieM <Leshaniebiz@gmail.com>
2024-12-05 22:00:52 +0530

    first commit

---
+++ src/app/pages/hall/hall.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hall',
+  imports: [],
+  templateUrl: './hall.component.html',
+  styleUrl: './hall.component.css'
+})
+export class HallComponent {
+
+}
