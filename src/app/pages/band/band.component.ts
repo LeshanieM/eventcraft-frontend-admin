50b7c7af854a11e6db451939ec39f4ce607bc2d6
LeshanieM <Leshaniebiz@gmail.com>
2024-12-09 03:08:13 +0530

    ui done

--- src/app/pages/band/band.component.ts
+++ src/app/pages/band/band.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../common/navbar/navbar.component';
 
 @Component({
   selector: 'app-band',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './band.component.html',
   styleUrl: './band.component.css'
 })
